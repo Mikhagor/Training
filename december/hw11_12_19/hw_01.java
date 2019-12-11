49be40d1a587acd9f5a40d506a388dcd5c367807
Yegor Mikhaylov <Emikhaylov@aplana.com>
2019-12-11 16:32:29 +0300

    hw1

--- december/hw11_12_19/hw_01.java
+++ december/hw11_12_19/hw_01.java
@@ -1,4 +1,10 @@
 package december.hw11_12_19;
 
 public class hw_01 {
+    public static void main(String[] args) {
+        for(int a = 1;a<5;a++) {
+            System.out.println("ololo");
+        }
+
+    }
 }
