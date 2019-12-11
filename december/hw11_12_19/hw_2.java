6448c6b9d3fd678822ca52ad5d2e61afb76fb911
Mikhagor <58772941+Mikhagor@users.noreply.github.com>
2019-12-11 22:30:01 +0300

    Create hw_2.java

---
+++ december/hw11_12_19/hw_2.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		String house = ("thtwfour");
+		String kv = ("eith");
+
+		if (house.equals("thtwfour"))  {
+
+			for (int a = 1; a < 5; a++)
+
+				System.out.println("home");
+
+		} else {
+			System.out.println("not hi");
+		}
+	}
+}
