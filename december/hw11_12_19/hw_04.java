37fc47d7e53d6c2eaeae1b5179c16982f98c663a
Mikhagor <58772941+Mikhagor@users.noreply.github.com>
2019-12-12 09:22:57 +0300

    Add files via upload

---
+++ december/hw11_12_19/hw_04.java
@@ -0,0 +1,29 @@
+package december.hw11_12_19;
+
+import java.util.Scanner; // подключаем сканер
+
+public class hw_04 {
+    public static void main(String[] args) {
+        System.out.print("Введите номер дома: ");
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        if (i == 8) {
+            for (int a = 0; a < 2; a++)
+            System.out.println("Верный номер дома");
+        } else {
+            System.out.println("Вы ввели не верный номер своего дома");
+        }
+
+        System.out.print("Введите номер квартиры: ");
+        Scanner input2 = new Scanner(System.in);
+        int i2 = input2.nextInt();
+        if (i2 == 324) {
+
+            System.out.println("Верный номер квартиры");
+        } else {
+            for (int a = 0; a < 9; a++)
+            System.out.println("Не верный номер квартиры");
+        }
+    }
+}
+
