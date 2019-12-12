608a96242c5ec684bc4a7703ade5ea9d36436807
Yegor Mikhaylov <Emikhaylov@aplana.com>
2019-12-12 09:16:48 +0300

    hw3-4

---
+++ december/hw11_12_19/hw_03.java
@@ -0,0 +1,19 @@
+package december.hw11_12_19;
+
+public class hw_03 {
+    public static void main(String[] args) {
+        String house = ("thtwfor");
+        String kv = ("eith");
+
+        if (house.equals("thtwfour"))  {
+
+            for (int a = 1; a < 5; a++)
+
+                System.out.println("home");
+
+        } else {
+            for (int a = 0; a < 3; a++)
+                System.out.println("not hi");
+        }
+    }
+}
---
+++ december/hw11_12_19/hw_2.java
@@ -0,0 +1,19 @@
+package december.hw11_12_19;
+
+public class hw_2 {
+            public static void main(String[] args) {
+            String house = ("thtwfor");
+            String kv = ("eith");
+
+            if (house.equals("thtwfour"))  {
+
+                for (int a = 1; a < 5; a++)
+
+                    System.out.println("home");
+
+            } else {
+                System.out.println("not hi");
+            }
+        }
+    }
+
