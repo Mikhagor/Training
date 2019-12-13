92d68d6c0b6dd95c0ee47630035510dd5d1307db
Yegor Mikhaylov <mikhagor@tutanota.com>
2019-12-13 12:12:57 +0300

    dfffg

--- hw.java
+++ hw.java
@@ -1,1 +1,10 @@
-oo
+package december.hw11_12_19;
+
+public class hw {
+    public static void main(String[] args) {
+        for(int a = 1;a<5;a++) {
+            System.out.println("ololo");
+        }
+
+    }
+}
