77f82944c529bcc9eda7e6a10f52984c2a4ec0f7
Yegor Mikhaylov <mikhagor@tutanota.com>
2019-12-17 16:34:10 +0300

    fdsg

--- Tasks/Task_17_12_2019.java
+++ Tasks/Task_17_12_2019.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task_17_12_2019 {
     public static void main(String[] args) {
         Scanner input1 = new Scanner(System.in);
-        int birthdate = input1.nextInt();
+        int N = input1.nextInt();
             for (int a = 0; a < 3; a++) {
                 System.out.println("Hello");
             }
