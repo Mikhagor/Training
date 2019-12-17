403803eed9ade8a487b3ede76a1a1ef53e05d3d7
Yegor Mikhaylov <mikhagor@tutanota.com>
2019-12-17 16:31:25 +0300

    fdsg

--- Tasks/Task_17_12_2019.java
+++ Tasks/Task_17_12_2019.java
@@ -3,16 +3,11 @@ package Tasks;
 import java.util.Scanner;
 
 public class Task_17_12_2019 {
-    public static void main (String[] args) {
-        System.out.print("Введите номер");
+    public static void main(String[] args) {
         Scanner input1 = new Scanner(System.in);
         int birthdate = input1.nextInt();
-        if (birthdate == 11) {
-            for (int a = 0; a < 3; a++)
+            for (int a = 0; a < 3; a++) {
                 System.out.println("Hello");
-        }
-        else {
-            System.out.println("Не верно");
+            }
         }
     }
-}
