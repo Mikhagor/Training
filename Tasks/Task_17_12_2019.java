32fd2c8a9a6c268700deda47c5c72fe641519aa9
Yegor Mikhaylov <mikhagor@tutanota.com>
2019-12-18 10:20:59 +0300

    fdsg

--- Tasks/Task_17_12_2019.java
+++ Tasks/Task_17_12_2019.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Task_17_12_2019 {
     public static void main(String[] args) {
-        Scanner input1 = new Scanner(System.in);
-        int N = input1.nextInt();
-            for (int a = 0; a < 3; a++) {
-                System.out.println("Hello");
-            }
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int a = 0; a < n; a++) {
+            System.out.println("Hello");
         }
     }
+}
